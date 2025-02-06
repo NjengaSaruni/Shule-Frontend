21dde16667106a12e5ba5131691814a63b9a9ee5
Peter Njenga <peternjengask@gmail.com>
2025-02-06 10:44:30 +0300

    Double displaying lazy loaded dashboard

---
+++ src/app/features/dashboard/dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './components/dashboard.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    DashboardComponent
+  ]
+})
+export class DashboardModule { }
